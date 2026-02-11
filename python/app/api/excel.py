2214a0bbb609e654ebffc86c9a078d081586b38b
demon7x <demon7x@westworld.co.kr>
2026-02-11 18:43:24 +0900

    filter object

--- python/app/api/excel.py
+++ python/app/api/excel.py
@@ -822,7 +822,8 @@ class ExcelWriteModel:
             self._excel_file  = "%s/scanlist_01.xls"%self._excel_path
         else:
             last = sorted(excel_files)[-1]
-            num = list(filter(str.isdigit,str(os.path.basename(last))))[0]
+            num = list(filter(str.isdigit,str(os.path.basename(last))))
+            print(num)
             new_name = "scanlist_%02d.xls"%(int(num)+1)
             self._excel_file = "%s/%s"%(self._excel_path,new_name)
 
