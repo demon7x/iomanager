eb2752716f8be032f6a6ff7511321288870518cf
demon7x <demon7x@westworld.co.kr>
2026-02-11 18:46:24 +0900

    filter object

--- python/app/api/excel.py
+++ python/app/api/excel.py
@@ -823,8 +823,8 @@ class ExcelWriteModel:
         else:
             last = sorted(excel_files)[-1]
             num = list(filter(str.isdigit,str(os.path.basename(last))))
-            print(num)
-            new_name = "scanlist_%02d.xls"%(int(num)+1)
+            num = max(map(int,num))
+            new_name = "scanlist_%02d.xls"%(num+1)
             self._excel_file = "%s/%s"%(self._excel_path,new_name)
 
     @classmethod    
